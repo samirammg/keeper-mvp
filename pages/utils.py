0428fa9da693d9b08f02bc77c9836bc825e300e1
samirammg <samira.golsefid@gmail.com>
2025-07-06 21:01:49 -0800

    Update utils.py

--- pages/utils.py
+++ pages/utils.py
@@ -1,16 +1,21 @@
 # utils.py
+
 import streamlit as st
 
 def hide_sidebar():
-    hide_sidebar_style = """
+    st.markdown("""
         <style>
-        [data-testid="stSidebarNav"] {
-            display: none !important;
-        }
-        [data-testid="stSidebar"] {
-            width: 0px !important;
-            padding: 0 !important;
-        }
+            [data-testid="stSidebar"] {
+                display: none !important;
+            }
+            [data-testid="stSidebarNav"] {
+                display: none !important;
+            }
+            [data-testid="collapsedControl"] {
+                display: none !important;
+            }
+            .main {
+                margin-left: 0 !important;
+            }
         </style>
-    """
-    st.markdown(hide_sidebar_style, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
